ddb6164439b82521e5ab379d742f730cb4e53d42
fahimahammed <fahimahammed.cse@gmail.com>
2022-04-05 00:07:53 +0600

    some charts added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import BarCharts from '../Charts/BarCharts';
+import DataComposedChart from '../Charts/DataComposedChart';
+import LineCharts from '../Charts/LineCharts';
+import TwoLevelPieChart from '../Charts/TwoLevelPieChart';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-            
+        <div className="container mx-auto">
+            <h1 className="text-4xl text-center mb-10">Dashboard</h1>
+            <div className='flex flex-wrap space-x-10'>
+                <LineCharts/>
+                <TwoLevelPieChart/>
+                <BarCharts/>
+                <DataComposedChart/>
+            </div>
         </div>
     );
 };
